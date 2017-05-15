a7ba8f3699a5bab193473b14d5ff5b69a8a07bed
skwang <skwang@princeton.edu>
2017-05-14 23:43:53 -0400

    selection

--- js/main.js
+++ js/main.js
@@ -32,6 +32,13 @@ var init_vel = 3;
 //var init_vel = 0.2;
 var collision_i;
 
+var isShiftDown;
+
+var selectGeo;
+var selectMaterial;
+var selectSphereMesh;
+var selectBoid;
+
 // compatability check before starting
 if (Detector.webgl) {
     init();
@@ -73,6 +80,7 @@ function init_boids_birds(boids, birds, xwing) {
         }
         bird.scale.set(2,2,2);
         bird.phase = Math.floor( Math.random() * 62.83 );
+        bird.boid_ref = boid;
         scene.add( bird );
     }
 }
@@ -129,6 +137,9 @@ function update_bullets(bullets, bullet_meshs, boids, birds) {
             var collide_pos = bullet.position;
             // TODO: do something here for an explosion when there is a collision
             scene.remove(birds[collision_i]);
+            if (boids[collision_i] == selectBoid) {
+                selectBoid = undefined;
+            }
             boids.splice(collision_i, 1);
             birds.splice(collision_i, 1);
             console.log('count: ' + boids.length);
@@ -359,6 +370,9 @@ function init() {
     // For selecting units
     raycaster = new THREE.Raycaster();
     mouse = new THREE.Vector2();
+    selectGeo = new THREE.SphereGeometry(15);
+    selectMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+    selectSphereMesh = new THREE.Mesh( selectGeo, selectMaterial);
 }
 
 
@@ -370,6 +384,11 @@ function render() {
     update_bullets(bullets_tie, bullet_meshs_tie, boids_xwing, birds_xwing);
 
     update_explosions(explosions);
+
+    if (selectBoid !== undefined) {
+        selectSphereMesh.position.copy(selectBoid.position);
+        console.log('still selected');
+    }
 
     renderer.render( scene, camera );
 }
@@ -390,23 +409,25 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function onKeyDown(e) {
-    if (e.shiftKey) {
-        // disable rotation
-        controls.enabled = false;
+function onKeyDown( event ) {
+    switch( event.keyCode ) {
+        case 16: 
+            isShiftDown = true; 
+            controls.enabled = false;
+            break;
     }
 }
-
-function onKeyUp(e) {
-    // 16 is key code for shift key
-    if (e.keyCode == 16) {
-        // renable rotation
-        controls.enabled = true;
+function onKeyUp( event ) {
+    switch ( event.keyCode ) {
+        case 16: 
+            isShiftDown = false; 
+            controls.enabled = true;
+            break;
     }
 }
 
 function onLeftClick(e) {
-    if (e.shiftKey) {
+    if (isShiftDown) {
         mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
         mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
 
@@ -417,18 +438,27 @@ function onLeftClick(e) {
         //console.log(scene.children);
         var intersects = raycaster.intersectObjects( birds_xwing );
         console.log(intersects.length);
+        var min_dist = Infinity;
+        var min_boid = undefined;
         for ( var i = 0; i < intersects.length; i++ ) {
-            console.log(intersects[i]);
-            intersects[ i ].object.material.color.set( 0xffffff );
-
+            if (intersects[i].distance < min_dist){
+                min_dist = intersects[i].distance;
+                min_boid = intersects[i].object.boid_ref;
+            }
         }
-        var intersects = raycaster.intersectObjects( birds_tie );
-        console.log(intersects.length);
-        for ( var i = 0; i < intersects.length; i++ ) {
-            console.log(intersects[i]);
-            intersects[ i ].object.material.color.set( 0xffffff );
-
+        if (min_boid !== undefined) {
+            selectBoid = min_boid;
+            console.log('selected');
+            selectSphereMesh.position.copy(selectBoid.position);
+            scene.add(selectSphereMesh);
         }
+        // var intersects = raycaster.intersectObjects( birds_tie );
+        // console.log(intersects.length);
+        // for ( var i = 0; i < intersects.length; i++ ) {
+        //     console.log(intersects[i]);
+        //     intersects[ i ].object.material.color.set( 0xffffff );
+
+        // }
     }
     else {
         for (var i = 0, il = boids_xwing.length; i < il; i++) {
